63adf7581534fe2264ad1920b64258964e6020df
jjHernandez17 <juanjohervar1708@gmail.com>
2025-04-30 21:43:39 -0500

    Add files via upload

--- Calificaciones.py
+++ Calificaciones.py
@@ -31,22 +31,34 @@ while wile_agregar_estudiante_menu:
         print("Estudiante agregado con exito ")
         print("---"*30)
         menu = int(input("--Que desea hacer-- \n(1) Ingresar otro estudiante \n(2) Asignar notas a estudiante \n"
-        "(3) Buscar esudiante \n(4) Listar estudiantes y mostrar notas\n"))
+        "(3) Buscar esudiante \n(4) Listar estudiantes y mostrar notas\n(5) Sacar promedio de estudiante/s\n"))
+        print ("---"*30)
         wile_agregar_estudiante_menu = True                 #se define esta variable verdadera porque para salir del while anterior lo definimos como falsa
         if menu == 1:                                       #si el estudiante escoge la opcion (1) agregar mas estudiantes 
             print("")
             wile_volver_menu = False                        #se quiebra el while de volver al menu pa que se devuelva al de crear estudiante
         elif menu == 2:                                     #si el usuario elige la opcion (2)  asignar notas a cada estudiante  
             notas_otro_estudiante = True                    #Se crea el while por si quiere agregarle notas a otro estudiante diferente del anterior
-            while notas_otro_estudiante:                    
-                nombre_buscar = input("Ingrese el nombre del estudiante para asignarle notas: ")
-                
+            while notas_otro_estudiante:  
+                nombre_no_encontrado = True   
+                while nombre_no_encontrado:               
+                    nombre_buscar = input("Ingrese el nombre del estudiante para asignarle notas: ")
+                    if not nombre_buscar in estudiantes:
+                        print("estudiante no encontrado o mal escrito, intentelo de nuevo")
+                        print("---"*30)
+                    elif nombre_buscar in estudiantes:
+                        nombre_no_encontrado = False
                 valores_nombre = estudiantes.get(nombre_buscar)         #en la variable valores nombre se guarda los valores que tiene cada estudiante en especifico(.get)      
                 print(nombre_buscar,valores_nombre)                     #se imprime el estudiante que buscò y la lista de notas
                 ingresar_mas_notas_while = True                         #se crea el while que permite y pregunta si desea ingresar mas notas para ese estudiante
                 while ingresar_mas_notas_while:
-                    nota = int(input("Ingrese la nota del estudiante"))         
-
+                    nota_no_valida = True
+                    while nota_no_valida:
+                        nota = int(input("Ingrese la nota del estudiante"))  
+                        if nota<0 or nota>100:
+                            print("esa nota no es valida, intentelo de nuevo")
+                        else: 
+                            nota_no_valida = False
                     estudiantes[nombre_buscar].append(nota)             #esto guarda la nota que ingresò en el estudiante en especìfico en su propia lista de notas
                     valores_nombre = estudiantes.get(nombre_buscar)     #nuevamente trae a la variable valores nombre la lista de notas de el estudiante para poder imprimirlas
                     print(nombre_buscar,valores_nombre)
@@ -57,24 +69,58 @@ while wile_agregar_estudiante_menu:
                        
                     elif ingresar_mas_notas == "no":
                         ingresar_mas_notas_while = False
-                        
-                    
+
                 ingresar_notas_otro_estudiante = input("Desea ingresar notas de otro estudiante? (si/no)\n")
+
                 if ingresar_notas_otro_estudiante== "si":
                     print("")
                 elif ingresar_notas_otro_estudiante == "no":
                     notas_otro_estudiante = False
         elif menu == 3: 
-            menu_buscar_nombre = input("Ingrese el nombre del estudiante que desea buscar: \n")
-            if menu_buscar_nombre in estudiantes:
-                mostrar_lista = estudiantes.get(menu_buscar_nombre)
-                print(menu_buscar_nombre , mostrar_lista)
-                
+            buscar_mas_estudiantes = True
+            while buscar_mas_estudiantes: 
+
+                estudiante_no_encontrado = True
+                while estudiante_no_encontrado:
+                    menu_buscar_nombre = input("Ingrese el nombre del estudiante que desea buscar: \n")
+                    if menu_buscar_nombre in estudiantes:
+                        mostrar_lista = estudiantes.get(menu_buscar_nombre)
+                        print(menu_buscar_nombre , mostrar_lista)
+                        estudiante_no_encontrado = False
+
+                    elif not menu_buscar_nombre in estudiantes:
+                        print("estudiante no encontrado o mal escrito, intentelo de nuevo")
+                        print("---"*30)
+
+                respuesta_no_valida = True
+                while respuesta_no_valida:
+                    desea_buscar_mas = input("Desea buscar mas estudiantes? (si/no) \n")
                     
+                    if desea_buscar_mas == "si":
+                        print("")
+                        respuesta_no_valida = False
+                    elif desea_buscar_mas == "no":
+                        respuesta_no_valida = False
+                        buscar_mas_estudiantes = False
+                    else:
+                        print("Esa respuesta no es valida")
+                        print("---"*30)
+     
         elif menu == 4:
             for nombre, lista_notas in estudiantes.items(): 
                 mostrar = estudiantes.get(nombre)
                 print(nombre , mostrar)
+
+        elif menu == 5:
+            promedio_estudiante = input("ingrese de que estudiante desea sacar el promedio: ")
+            lista_pa_promedio = estudiantes.get(promedio_estudiante)
+            cantidad_notas = len(lista_pa_promedio)
+            notas_sumadas = 0
+            for conta in lista_pa_promedio:
+                notas_sumadas = notas_sumadas + conta
+            promedio_total_estudiante = notas_sumadas / cantidad_notas
+            print (f"el promedio de todas las notas de {promedio_estudiante} es: {promedio_total_estudiante}")
+
                 
 
 
@@ -82,13 +128,6 @@ while wile_agregar_estudiante_menu:
 
         
 
-
-            # if nombre_buscar in estudiantes:
-            #     print(estudiantes[nombre_buscar])
-            #     nota = int(input("ingrese la nota del estudiante"))
-            #     estudiante[lista_notas.append(nota)]
-            #     print(estudiantes[estudiante])
-                
 
         
 
